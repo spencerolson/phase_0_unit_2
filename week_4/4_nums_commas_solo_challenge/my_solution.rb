9c4e3d17eb69e8e303a3b76646e6b8c38993eeaa
Spencer Olson <spence1818@gmail.com>
2014-04-27 20:22:15 -0500

    Added reflection for Challenge 4

--- week_4/4_nums_commas_solo_challenge/my_solution.rb
+++ week_4/4_nums_commas_solo_challenge/my_solution.rb
@@ -7,12 +7,38 @@
 
 # 1. Pseudocode
 
-# What is the input?
-# What is the output? (i.e. What should the code return?)
+# What is the input? An integer number
+# What is the output? (i.e. What should the code return?) a String version of the number with commas
 # What are the steps needed to solve the problem?
+# 1. Convert the input number to a String
+# 2. If the input number is less than 1000, return a String version of that same number
+# 3. If the input number is greater than or equal to 1000, add commas in the appropriate spots and return a String version of the number.
+
 
 
 # 2. Initial Solution
+
+# def separate_comma (int_num)
+#     if int_num.to_s.length <=3 then
+#         return int_num.to_s 
+#     else
+        
+#         num_with_commas = ""
+#         current_digit_index = 0
+#         int_num.to_s.reverse.split("").each do |digit|
+#             if  current_digit_index != 0 && current_digit_index % 3 == 0
+#                 num_with_commas << ","
+#             end
+#             num_with_commas << digit
+#             current_digit_index += 1
+#         end
+        
+#         return num_with_commas.reverse
+#     end    
+# end
+
+
+# 3. Refactored Solution
 
 def separate_comma (int_num)
     if int_num.to_s.length <=3 then
@@ -29,13 +55,10 @@ def separate_comma (int_num)
             current_digit_index += 1
         end
         
-        return num_with_commas.reverse
+        num_with_commas.reverse
     end    
 end
-
-
-# 3. Refactored Solution
-
-
-
+# My refactored solution is nearly the same as the above code in the initial solution because I refactored on-the-fly. The one change I did make in this section was to get rid of the second 'return' statement, since Ruby will return that value anyways if it's the last thing that's evaluated.
 # 4. Reflection 
+
+# I liked this challenge because I took a good amount of creativity on my part to come up with a solution. I think the biggest 'aha' moment that I has was when I realized I could reverse the string and go through it left to right, instead of trying to figure out a way to traverse the string from right to left. I also hit a snag at one point because I was setting num_with_commas to equal an Array instead of a string, and this was causing my returned value to be an array, so it was failing the tests. A quick change from num_with_commas = [] to num_with_commas = "" fixed my problem.
