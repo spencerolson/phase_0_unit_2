260d994a96488e53c0b1cb71edb16cbf3041feb1
Spencer Olson <spence1818@gmail.com>
2014-05-04 15:36:41 -0500

    Challenge 1 Completed
    Finished pseudocode and reflection.

--- week_5/1_die/my_solution.rb
+++ week_5/1_die/my_solution.rb
@@ -5,9 +5,13 @@
 
 # 2. Pseudocode
 
-# Input:
-# Output:
-# Steps:
+# Input: An integer representing the number of sides on the die
+# Output: .sides returns an integer representing the number of sides on the die, .roll returns a random integer between 1 and the number of sides
+# Steps: 
+1. Create the .initialize method, which takes 1 argument -- the number of sides on the die.
+2. Within the .initialize method, raise an ArgumentError if the number of sides is less than 1. Otherwise, set a local variable @sides equal to the number of sides input.
+3. Create the .sides method, which returns the instance variable @sides
+4. Create the .roll method, which returns a random number between 1 and @sides, inclusive.
 
 
 # 3. Initial Solution
@@ -69,3 +73,4 @@ test_die3 = Die.new(20)
 puts "Test 3: Returns a number between 1 and 20 for a 20-sided die: #{test_die3.roll.between?(1,20)}"
 
 # 5. Reflection
+# In this challenge, I learned how to raise an ArgumentError and how to add a custom error message. In addition, I learned how to create test code for errors, using a begin-rescue-else statement. We got stuck for a bit on the rand function, because we weren't sure if it was inclusive or exclusive. After some research we found that using the syntax 1..@sides will include both 1 and @sides.
